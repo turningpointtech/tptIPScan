848b794a5d525795393049a6bd61d83d1791d92f
JoshTPT <josh@tptech.com.au>
2016-10-27 11:46:38 +1000

    Initial commit

---
+++ tptIPScan/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace tptIPScan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ipRange = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.ipRange);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(538, 45);
+            this.panel1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(293, 24);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(159, 10);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(290, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "0 of 254 Addresses Scanned";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Scan IP Range";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ipRange
+            // 
+            this.ipRange.Location = new System.Drawing.Point(73, 12);
+            this.ipRange.Name = "ipRange";
+            this.ipRange.Size = new System.Drawing.Size(100, 20);
+            this.ipRange.TabIndex = 4;
+            this.ipRange.Text = "10.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "IP Range:";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 45);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(538, 343);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "State";
+            this.columnHeader1.Width = 65;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "IP Address";
+            this.columnHeader2.Width = 130;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Hostname";
+            this.columnHeader3.Width = 150;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Delay";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 388);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TPT IP Scanner";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ipRange;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ tptIPScan/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.NetworkInformation;
+
+namespace tptIPScan
+{
+    public partial class Form1 : Form
+    {
+        public string[] ipList = new string[254];
+        public Dictionary<int, Thread> ipThreads;
+        public int ipsUp = 0;
+        public int threadsDone = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            generateIPList();
+            generateGridList();
+            startIPScan();
+        }
+
+        public void startIPScan()
+        {
+            int i = 0;
+            threadsDone = 0;
+            ipsUp = 0;
+            ipThreads = new Dictionary<int, Thread>();
+            foreach (string ip in ipList)
+            {
+                ParameterizedThreadStart tStart = new ParameterizedThreadStart(scanThread);
+                ipThreads[i] = new Thread(tStart);
+                object objarr = new object[2] { i, ip };
+                ipThreads[i].Start(objarr);
+                i++;
+            }
+        }
+
+        public void scanThread(object args)
+        {
+            Array argArray = new object[2];
+            argArray = (Array)args;
+
+            int id = (int)argArray.GetValue(0);
+            string ipa = (string)argArray.GetValue(1);
+            
+            IPAddress ipaddr = IPAddress.Parse(ipa);
+            bool pingable = false;
+            long delay;
+            Ping pinger = new Ping();
+            try
+            {
+                PingReply reply = pinger.Send(ipaddr);
+                pingable = reply.Status == IPStatus.Success;
+                if (pingable) {
+                    delay = reply.RoundtripTime;
+                    listView1.Items[id].Text = "UP!";
+                    ipsUp++;
+                    listView1.Items[id].SubItems[3].Text = Convert.ToString(delay) + "ms";
+                    try
+                    {
+                        IPHostEntry hostInfo = Dns.GetHostEntry(ipa);
+                        if (hostInfo != null)
+                        {
+                            listView1.Items[id].SubItems[2].Text = hostInfo.HostName;
+                        }
+                        else
+                        {
+                            listView1.Items[id].SubItems[2].Text = "N/A";
+                        }
+                    }
+                    catch(Exception)
+                    {
+                        listView1.Items[id].SubItems[2].Text = "N/A";
+                    }
+                }
+                else
+                {
+                    listView1.Items[id].Text = "DOWN";
+
+                }
+            } catch (PingException)
+            {
+                listView1.Items[id].Text = "DOWN";
+            }
+            threadsDone++;
+            if (threadsDone > 252)
+            {
+                progressBar1.Value = 254;
+                label1.Text = Convert.ToString(ipsUp) + " Addresses UP";
+            }
+            else
+            {
+                label1.Text = Convert.ToString(threadsDone) + " of 254 Addresses Scanned";
+                progressBar1.Maximum = 254;
+                progressBar1.Value = threadsDone;
+            }
+        }
+
+        public void generateIPList()
+        {
+            Array.Clear(ipList, 0, ipList.Length);
+            for (int i = 0; i < 254; i++)
+            {
+                ipList[i] = ipRange.Text + "." + Convert.ToString(i+1);
+            }
+        }
+
+        public void generateGridList()
+        {
+            listView1.Items.Clear();
+            foreach (string ip in ipList)
+            {
+                ListViewItem itm = new ListViewItem();
+                itm.Text = "Trying";
+                itm.SubItems.Add(ip);
+                itm.SubItems.Add("");
+                itm.SubItems.Add("");
+                listView1.Items.Add(itm);
+            }
+        }
+    }
+}
